f07b1e4646c091aa626bcb1f80aa80e5c354688f
Garima Soni <garimasoni@gmail.com>
2026-08-15 20:45:49 +0530

    Debug Cloudinary configuration

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -6,4 +6,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloudinary config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key_exists: !!process.env.CLOUDINARY_API_KEY,
+  api_secret_exists: !!process.env.CLOUDINARY_API_SECRET,
+});
+
 export default cloudinary;
