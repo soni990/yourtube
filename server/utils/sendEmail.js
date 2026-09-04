4a35307291bd0409e437e7bd4b6f93f3e7a9c6c2
Garima Soni <garimasoni@gmail.com>
2026-09-04 14:46:25 +0530

    connection

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,4 +1,6 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (email, subject, message) => {
   const transporter = nodemailer.createTransport({
